f71d591751f7cb34fe50d6a95dd95d9ccc8327e1
Rizkianugrah <46855274+Rizkianugrah@users.noreply.github.com>
2022-07-21 14:51:54 +0700

    Update ini-scbot.js

--- plugins/ini-scbot.js
+++ plugins/ini-scbot.js
@@ -3,7 +3,7 @@ let handler = async(m, { conn, text, usedPrefix, command }) => {
         let pp = await conn.profilePictureUrl(m.chat).catch(_ => null)
 let pepe = pp ? await (await fetch(pp)).buffer() : Buffer.alloc(0)
 let str = `*NYARI ESCE ?*\nSilahkan dibawah ,Terimakasih`
-conn.sendHydrated(m.chat, str, wm, pepe, 'https://xnxx.com/', '𝙶𝙸𝚃𝙷𝚄𝙱', null, null, [
+conn.sendHydrated(m.chat, str, wm, pepe, 'https://194.233.66.232/', '𝙶𝙸𝚃𝙷𝚄𝙱', null, null, [
 ['𝙼𝙴𝙽𝚄', '/menu']
 ['\n sᴇʙᴇɴᴀʀɴʏᴀ ɢᴡ sᴀɴɢᴇ\nᴊᴅɪ ᴍᴀ ɢᴀ ɴɢᴡᴇ sᴀᴍᴀ ɢᴡ?','tq']
 ], m)
